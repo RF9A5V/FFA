c187c4bc617c4423f7b2d928384e93ceebd4da90
Jason Chiu <jforeza@gmail.com>
2015-11-21 08:36:04 -0800

    Added basic login placeholder

--- www/js/controllers/loginController.js
+++ www/js/controllers/loginController.js
@@ -1,3 +1,13 @@
 /**
  * Created by Dooshkukakoo on 11/21/2015.
  */
+ffe.controller('loginController', ['$scope','$state',
+	function($scope, $state) {
+
+
+	$scope.backToHome = function() {
+    	$state.go("home");
+  	};
+
+
+}]);
